a5a059bafd272ca7a574149e58b1a8733aab09d8
Olga Solodovnikova <kunizza@yandex.ru>
2024-02-27 13:35:32 +0300

    Проектная работа__6

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,13 +1,13 @@
 export { openPopup, closePopup };
 
-// Функция добавления класса открытия формы
+// Функция открытия окна
 function openPopup(popupInfo) {
   popupInfo.classList.add("popup_is-opened", "popup_is-animated");
   popupInfo.addEventListener("click", formCloseOverlay);
   document.addEventListener("keydown", formCloseEsc);
 }
 
-// Функция удаления класса открытия формы
+// Функция закрытия окна
 function closePopup(popupInfo) {
   popupInfo.classList.remove("popup_is-opened");
   popupInfo.removeEventListener("click", formCloseOverlay);
@@ -19,11 +19,11 @@ const formCloseOverlay = (evt) => {
   if (evt.target === evt.currentTarget) {
     closePopup(evt.target);
   }
-};
+}
 
 // Закрытие окна кнопкой Esc
 const formCloseEsc = (evt) => {
   if (evt.key === "Escape") {
     closePopup(document.querySelector(".popup_is-opened"));
   }
-};
+}
