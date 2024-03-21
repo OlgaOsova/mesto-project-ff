2f068f0def956a4bdbc8ce056f14eb3e92856be7
Olga Solodovnikova <kunizza@yandex.ru>
2024-03-21 07:33:10 +0300

    Проектная_работа_7

--- src/components/api.js
+++ src/components/api.js
@@ -5,7 +5,7 @@ export {
   addLike,
   deleteLike,
   updateAvatar,
-  deleteCard,
+  deleteCard
 }
 
 // Конфигурация API
@@ -13,8 +13,8 @@ const config = {
   baseUrl: "https://nomoreparties.co/v1/wff-cohort-8",
   headers: {
     authorization: "fda0d25e-1116-4785-b3a1-842d34e7432a",
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 }
 
 // Универсальная функция для проверки и получения данных ответа
@@ -25,24 +25,18 @@ const getResponseData = (res) => {
   return Promise.reject(`Ошибка: ${res.status}`);
 }
 
-// Универсальная функция запроса с проверкой ответа
-const request = (url, options) => {
-  return fetch(`${config.baseUrl}${url}`, options)
-  .then(getResponseData);
-}
-
 // Получение карточек с сервера
 const getCard = () => {
-  return request(`/cards`, {
-    headers: config.headers
-  });
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 }
 
 // Получение информации о пользователе с сервера
 const getProfile = () => {
-  return request(`/users/me`, {
-    headers: config.headers
-  });
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 }
 
 // Параллельный запуск и выполнение созданных промисов
@@ -57,45 +51,45 @@ const updateProfile = (name, about) => {
     headers: config.headers,
     body: JSON.stringify({
       name: name,
-      about: about
+      about: about,
     }),
-  });
+  }).then((res) => getResponseData(res));
 }
 
-// Добавление новой карточки из формы "Новое место"
+// Добавление новой карточки из формы "Новое"
 const updateCard = (name, link) => {
   return fetch(`${config.baseUrl}/cards`, {
     method: "POST",
     headers: config.headers,
     body: JSON.stringify({
       name: name,
-      link: link
+      link: link,
     }),
-  });
+  }).then((res) => getResponseData(res));
 }
 
 // Постановка лайка карточки
 const addLike = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
     method: "PUT",
-    headers: config.headers
-  });
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 }
 
 // Снятие лайка карточки
 const deleteLike = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
     method: "DELETE",
-    headers: config.headers
-  });
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 }
 
 // Удаление карточки пользователя
 const deleteCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/${cardId}`, {
     method: "DELETE",
-    headers: config.headers
-  });
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
 }
 
 // Обновление аватара пользователя
@@ -104,7 +98,7 @@ const updateAvatar = (avatar) => {
     method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
-      avatar: avatar
-    })
-  });
+      avatar: avatar,
+    }),
+  }).then((res) => getResponseData(res));
 }
